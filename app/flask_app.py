fabcf4d22abfc18dbd2a3381e9b83811999630ae
Sandeep Joshi <sandeep.combat@gmail.com>
2025-01-27 07:50:30 +0000

    lint issueS

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,12 +1,16 @@
+import mlflow
 from flask import Flask, request, jsonify
-import mlflow.sklearn
 
 app = Flask(__name__)
 
-# Load the model from MLflow
-model = mlflow.sklearn.load_model(
-    "runs:/fa77e9f876e5497d908ecdb1f381749f/model"
-    )
+# Load the model from the mounted path
+model_path = (
+    "/app/mlruns/158105573897580942/"
+    "fa77e9f876e5497d908ecdb1f381749f/"
+    "artifacts/model"
+)
+model = mlflow.sklearn.load_model(model_path)
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -14,7 +18,6 @@ def predict():
     features = data["features"]
     prediction = model.predict([features])
     return jsonify({"prediction": int(prediction[0])})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
