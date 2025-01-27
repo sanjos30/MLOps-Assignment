7c75c682a81c3f7566a029f540b1c188666fcf5d
Sandeep Joshi <sandeep.combat@gmail.com>
2025-01-27 07:42:56 +0000

    lint error fix

--- app/flask_app.py
+++ app/flask_app.py
@@ -4,7 +4,8 @@ import mlflow.sklearn
 app = Flask(__name__)
 
 # Load the model from MLflow
-model = mlflow.sklearn.load_model("runs:/fa77e9f876e5497d908ecdb1f381749f/model")
+model = mlflow.sklearn
+.load_model("runs:/fa77e9f876e5497d908ecdb1f381749f/model")
 
 
 @app.route("/predict", methods=["POST"])
